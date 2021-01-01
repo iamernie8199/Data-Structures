cb0b55f6476eaf4ad02c98798acfb1456a59ecd4
iamernie8199 <iamernie8199@gmail.com>
2021-01-02 02:30:20 +0800

    失敗場次存檔

--- Project3/source/match.bat
+++ Project3/source/match.bat
@@ -6,8 +6,10 @@ set /a numofgame=30
 IF NOT (%1)==() (set /a numofgame=%1)
 for /L %%n in (1 1 %numofgame%) do (
 a.exe > output
-find "Red Player won the game" .\output>nul && set /a red+=1 && echo Red won
-find "Blue Player won the game" .\output>nul && set /a blue+=1 && echo Blue won
+find "Red Player won the game" .\output>nul && set /a red+=1 && echo R
+find "Red_Player is winner." .\output>nul && set /a red+=1 && echo B violated
+find "Blue Player won the game" .\output>nul && set /a blue+=1 && echo B && copy output %%n
+find "Blue_Player is winner." .\output>nul && set /a blue+=1 && echo R violated
 )
 echo %numofgame% games
 echo Red wins %red%
