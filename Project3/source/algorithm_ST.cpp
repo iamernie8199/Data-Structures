13541f63a90534efa46635c710e5c884d0be074d
iamernie8199 <iamernie8199@gmail.com>
2021-01-03 08:02:33 +0800

    精簡

--- Project3/source/algorithm_ST.cpp
+++ Project3/source/algorithm_ST.cpp
@@ -3,10 +3,6 @@
 #include <time.h>
 #include "../include/algorithm.h"
 
-
-#include <stack>
-
-
 using namespace std;
 
 /******************************************************
@@ -60,91 +56,12 @@ int** neighbors(int m, int n){
     return neighbor;
 }
 
-class B{
-    private:
-        int btmp[5][6];
-        Board state;
-        int new_move;
-    public:
-        B(Board board){
-            state = board;
-            new_move = 1;
-            for (int i = 0; i < 5; i++){
-                for (int j = 0; j < 6;j++){
-                    btmp[i][j] = board.get_orbs_num(i, j);
-                    if (board.get_cell_color(i, j) == 'b') btmp[i][j] = -btmp[i][j];
-                }
-            }
-        }
-        int get_num(int i, int j){
-            return abs(btmp[i][j]);
-        }
-        void set(int i, int j, int val){
-            btmp[i][j] = val;
-        }
-        char get_color(int i, int j){
-            if(btmp[i][j]> 0) return 'r';
-            else if(btmp[i][j]< 0) return 'b';
-            else return 'w';
-        }
-        void update(int m, int n, Player * player){
-            state.place_orb(m,n,player);
-            for (int i = 0; i < 5; i++){
-                for (int j = 0; j < 6;j++){
-                    btmp[i][j] = state.get_orbs_num(i, j);
-                    if (state.get_cell_color(i, j) == 'b') btmp[i][j] = -btmp[i][j];
-                }
-            }
-        }
-        Board get_board(){ return state;}
-};
-
-
-
-int* chain(Board board, Player player){
-    B btmp(board);
-    char color = player.get_color();
-    int lengths[30];
-    int tmp = 0;
-    for(int i = 0; i < ROW; i++){
-        for(int j = 0; j < COL; j++){
-            int l = 0;
-            if((btmp.get_num(i, j) == board.get_capacity(i, j)-1) && (btmp.get_color(i, j) == color)){
-                stack<int*> visiting_stack;
-                int position[2] = {i, j};
-                visiting_stack.push(position);
-                while (!visiting_stack.empty()){
-                    btmp.set(visiting_stack.top()[0],visiting_stack.top()[1],0);
-                    visiting_stack.pop();
-                    l+=1;
-                    int** neighbor = neighbors(i, j);
-                    int num = near(i, j);
-                    for (int z = 0; z < num; z++){
-                        if ((btmp.get_num(neighbor[z][0], neighbor[z][1])==board.get_capacity(i, j)-1)&&(btmp.get_color(i, j) == color)){
-                            position[0] = neighbor[z][0];
-                            position[1] = neighbor[z][1];
-                            visiting_stack.push(position);
-                        }
-                    }
-                }
-            }
-            lengths[tmp] = l;
-            tmp++;
-        }
-    }
-    return lengths;
-}
-
 
 int evaluate(Board board, Player player){
-    int score = 0;
-    int my_orbs = 0, enemy_orbs = 0;
-
+    int score = 0, my_orbs = 0, enemy_orbs = 0;
     char color = player.get_color();
     char enemy_color;
-    if (color=='r') enemy_color='b';
-    else enemy_color='r';
-
+    enemy_color=(color=='r')? 'b':'r';
     bool safe;
     for(int i = 0; i < ROW; i++){
         for(int j = 0; j < COL; j++){
@@ -179,80 +96,13 @@ int evaluate(Board board, Player player){
     score += my_orbs;
     if (enemy_orbs == 0 && my_orbs > 1) return 10000;
     if (enemy_orbs > 1 && my_orbs == 0) return -10000;
-    int* chain_l = chain(board, player);
-    for (int i = 0; i <30; i++){
-        if (chain_l[i]>1) score += 2*chain_l[i]; 
-    }
     return score;
-}
-
-class bestnode{
-    private:
-        int row;
-        int col;
-        int score;
-    public:
-        bestnode(){
-            row=0; col=0; score=0;
-        }
-        int get_score(){return score;}
-        int get_row(){ return row;}
-        int get_col(){ return col;}
-        void set_score(int sc){score=sc;}
-        void set_position(int x,int y){row=x;col=y;}
-};
-
-bestnode minimax(Board board, int depth,bool max, Player player){
-    char color = player.get_color();
-    char enemy_color = (color=='r')? 'b':'r';
-    if(max){
-        bestnode best;
-        best.set_score(-10000);
-
-        for(int i=0;i<5;i++){
-            for(int j=0;j<6;j++){
-                if(board.get_cell_color(i,j)!=enemy_color){
-                    Board thisround=board;
-                    thisround.place_orb(i,j,&player);
-                    bestnode tmp = minimax(thisround, depth+1, false, player);
-                    tmp.set_position(i, j);
-                    if (tmp.get_score()>best.get_score()) best = tmp;
-                }
-            }
-        }
-        return best;
-    }
-    else{
-        bestnode best;
-        best.set_score(10000);
-        for(int i=0;i<5;i++){
-            for(int j=0;j<6;j++){
-                if(board.get_cell_color(i,j)!=color){
-                    Board thisround=board;
-                    Player enemy(enemy_color);
-                    thisround.place_orb(i,j,&enemy);
-                    bestnode tmp = minimax(thisround, depth+1, false, player);
-                    tmp.set_position(i, j);
-                    if (tmp.get_score()<best.get_score()) best = tmp;
-                }
-            }
-        }
-        return best;
-    }
-    
 }
 
 void algorithm_A(Board board, Player player, int index[]){
     srand(time(NULL)*time(NULL));
     int row, col;
     char color = player.get_color();
-    /*
-    Board thisround = board;
-    bestnode best = minimax(thisround,0,true,player);
-    index[0] = best.get_row();
-    index[1] = best.get_col();
-    cout << index[0] << " " << index[1] << endl;
-    */
     int corners[4][2] = {
         {0,0},{0,5},{4,0},{4,5}
     };
@@ -281,7 +131,7 @@ void algorithm_A(Board board, Player player, int index[]){
             i++;
         }
         else{
-            row = rand() % 5;
+            /*row = rand() % 5;
             col = rand() % 6;
             for (int i = 0; i < 4; i++){
                 while(row==corners[i][0] && col==corners[i][1]){
@@ -289,31 +139,31 @@ void algorithm_A(Board board, Player player, int index[]){
                     col = rand() % 6;
                 }
             }
-            /*Board thisround = board;
+            Board thisround = board;
             thisround.place_orb(row,col,&player);
             int score = evaluate(thisround, player);*/
+            int score = 0;
             for(int i=0;i<ROW;i++){
                 for(int j=0;j<COL;j++){
                     if(board.get_cell_color(i,j)==color){
                         Board tmpround = board;
                         tmpround.place_orb(i,j,&player);
                         int tmpscore = evaluate(tmpround, player);
-                        if (tmpscore==10000){
+                        if (tmpscore>score){
                             row=i;
                             col=j;
-                            //score = tmpscore;
+                            score = tmpscore;
                         }
-                        /*if((board.get_capacity(i,j)-board.get_orbs_num(i,j))<=4){
+                        else if(tmpscore==10000){
                             row=i;
                             col=j;
-                        }*/
+                        }
                     }
                 }
             }
             if (board.get_cell_color(row, col) == color || board.get_cell_color(row, col) == 'w') break;
         }
     }
-    
     index[0] = row;
     index[1] = col;
 }
